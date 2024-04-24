7a5eb5b38fd01625d96490c81753305315cb2963
Douglas Landvik <douglas.landvik@gmail.com>
2024-04-24 22:35:14 +0200

    chore(pubsub): add withDeadletterWihoutSubscrption

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,7 +4,6 @@ import { WelcomeAi } from "@/auth/welcome";
 
 export default function Home() {
 	const [message, setMessage] = useState("");
-
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<WelcomeAi isUserLoggedIn={false} isMobile={true}/>
