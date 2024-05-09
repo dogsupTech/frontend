018d00abf3625a5fdb1de15a92eaebc80b12116d
Douglas Landvik <douglas.landvik@gmail.com>
2024-05-10 01:11:15 +0200

    DBAI-112 feat(frontend): add frontend auth

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,6 +6,8 @@ import { WelcomeAi } from "@/components/auth/welcome";
 import initTranslations from '../i18n';
 import TranslationsProvider from '@/components/TranslationsProvider';
 import { useIsMobile } from "@/useIsMobile";
+import { useAuth } from "@/components/auth/auth";
+import { object } from "zod";
 
 const i18nNamespaces = ['default'];
 
@@ -23,6 +25,7 @@ export default function Home({params: {locale}}: HomeProps) {
 			setTranslations(trans);
 		});
 	}, [locale]);
+	const {user, userData, isLoading, refreshUserData} = useAuth();
 
 	const [translations, setTranslations] = useState<{ t: Function, resources: any } | null>(null);
 	
@@ -32,14 +35,28 @@ export default function Home({params: {locale}}: HomeProps) {
 	
 	const {t, resources} = translations;
 
-
 	return (
 		<TranslationsProvider
 			namespaces={i18nNamespaces}
 			locale={locale}
 			resources={resources}>
 			<main className={"border-2 h-screen items-center justify-center"}>
-				<WelcomeAi isUserLoggedIn={false} isMobile={isMobile}/>
+				{ 
+					!user && !isLoading && !userData ? <WelcomeAi isUserLoggedIn={false} isMobile={isMobile}/> :
+						// dog things
+					<div>
+						{
+							Object.entries(userData!).map(([key, value]) => { 
+								return (
+									<div key={key}>
+										{key}: {value}
+									</div>
+								)
+							})
+						}
+						
+					</div>
+				}
 			</main>
 		</TranslationsProvider>
 	);
